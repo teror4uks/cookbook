8a050a42c0598b2976806b4bd65e9c28f396b2a6
Pavel Sirotkin <pav.pnz@gmail.com>
2018-12-30 17:30:52 +0300

    add protocol implementation

--- crazy_notes/link_list.py
+++ crazy_notes/link_list.py
@@ -93,11 +93,83 @@ class LinkedList:
 
 
 class PLinkedList:
+    """
+    >>> from link_list import LinkedList
+    >>> pl = PLinkedList()
+    >>> [pl.add(i) for i in range(10)]
+    [None, None, None, None, None, None, None, None, None, None]
+    >>> print(pl)
+    [1, 2, 3, 4, 5, 6, 7, 8, 9]
+    >>> pl[4] = 100
+    >>> print(pl)
+    [1, 2, 3, 4, 100, 6, 7, 8, 9]
+    >>> pl.delete(3)
+    >>> print(pl)
+    [1, 2, 3, 100, 6, 7, 8, 9]
+    """
     def __init__(self):
         self.head = LinkedNode()
 
-    def __setitem__(self, key, value):
-        pass
+    def __setitem__(self, index, value):
+        if not isinstance(index, int):
+            raise Exception('Index must be int')
+
+        new_node = LinkedNode(value)
+
+        if index == 0:
+            h_next = self.head.next
+            self.head = new_node
+            self.head.next = h_next
+            return
+
+        node = self.head
+
+        for i in range(index - 1):
+            node = node.next
+
+        new_node.next = node.next.next
+
+        node.next = new_node
+
+    def __iter__(self):
+        node = self.head
+
+        while node.next:
+            yield node
+            node = node.next
+
+        yield node
+
+    def delete(self, index):
+        if index == 0:
+            self.head = self.head.next
+            return
+
+        node = self.head
+
+        for i in range(index-1):
+            node = node.next
+
+        deleted_node = node.next
+        node_after_deleted = deleted_node.next
+        node.next = node_after_deleted
+
+    def add(self, data):
+        node = LinkedNode(data)
+
+        if not self.head.next and not self.head.data:
+            self.head = node
+            return
+
+        temp = self.head
+
+        while temp.next:
+            temp = temp.next
+
+        temp.next = node
+
+    def __str__(self):
+        return '{}'.format(list(self))
 
 
 if __name__ == '__main__':
